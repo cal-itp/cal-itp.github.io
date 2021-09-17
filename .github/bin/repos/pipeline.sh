8b6b0be4d8c0fb41412151ca0a290479c38c946b
Kegan Maher <kegan@compiler.la>
2021-09-17 18:07:41 +0000

    chore(pipeline): more descriptive comments

--- .github/bin/repos/pipeline.sh
+++ .github/bin/repos/pipeline.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 set -eu
 
+# run the repository data pipeline: get > filter > sort > output
+
 source=.github/bin/repos
 target=_data/repos.json
 
 $source/get.sh | $source/filter.sh | $source/sort.sh > "$target"
+
+# save resulting file name in GitHub Actions context
+# see https://docs.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-an-output-parameter
 
 echo "::set-output name=data_file::$target"
